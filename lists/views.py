eee08ef5457c2e82dd369d78b07070ed1f228c41
Moisès Gómez Girón <moises@blamix230.blami.net>
2019-10-16 16:54:51 +0200

    now there's a title in the home page

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home_page():
-    pass
+def home_page(request):
+    contents = '<html><title>To-Do list</title></html>'
+    return HttpResponse(contents)
 
