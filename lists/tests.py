f0e23685d1b24f2f03b3e2d81d7ea9f6897f01e9
Moisès Gómez Girón <moises@blamix230.blami.net>
2019-10-24 16:10:34 +0200

    now it is able to show all the items

--- lists/tests.py
+++ lists/tests.py
@@ -33,6 +33,15 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
 
