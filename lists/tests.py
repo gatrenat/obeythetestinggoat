a4900b91158d2cc37416fd01d991ff779fa51b71
Moisès Gómez Girón <moiatgit@gmail.com>
2019-10-28 16:36:15 +0100

    removed test_only_saves_​items_when_necessary()

--- lists/tests.py
+++ lists/tests.py
@@ -20,16 +20,6 @@ class HomePageTest(TestCase):
         self.assertIn('itemey 1', response.content.decode())
         self.assertIn('itemey 2', response.content.decode())
 
-    #def test_redirects_after_POST(self):
-    #    response = self.client.post('/', data={'item_text': 'A new list item'})
-    #    #self.assertEqual(response.status_code, 302)
-    #    #self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-    #    self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ListViewTest(TestCase):
 
